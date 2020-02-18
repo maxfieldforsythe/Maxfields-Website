166d9aa9bf2e329ff2fa7c04ee1a31279ccc127d
Max Forsythe <maxfieldforsythe@gmail.com>
2020-02-18 01:02:47 -0700

    basic transitioning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom'
+import { useRef, useState, useEffect, useCallback } from 'react'
+import { useTransition, animated } from 'react-spring'
+
+
+const pages = [
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>M</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>A</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>X</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>F</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>I</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>E</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>L</animated.div>,
+  ({ style }) => <animated.div style={{ ...style, background: 'darkgrey' }}>D</animated.div>,
+ 
+  
+]
 
 function App() {
+  const [index, set] = useState(0)
+  const onClick = useCallback(() => set(state => (state + 1) % 8), [])
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 1, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 1, transform: 'translate3d(-100%,0,0)' },
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="simple-trans-main" onClick={onClick}>
+      {transitions.map(({ item, props, key }) => {
+        const Page = pages[item]
+        return <Page key={key} style={props} />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
