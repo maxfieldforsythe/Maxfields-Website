fc69f4dd28a3fe196f1c4227c87ecc3b5205d4aa
Max Forsythe <maxfieldforsythe@gmail.com>
2020-07-12 13:19:04 -0400

    Changed up website to page for my fathers store

--- src/About.js
+++ src/About.js
@@ -3,22 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './NavBar.js';
 import React, {Component, memo, useState , Fragment, Button} from 'react'
 
-import About from './About.js'
-import Shop from './Shop.js'
-
 
 class App extends Component {
   render() {
     return(
-      <React.Fragment>
-  <Router>
-    <NavigationBar />
-    <Switch>
-      <Route path="/shop" component={Shop} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </Router>
-</React.Fragment>
+        <p> Peter Forsythe's bio and website info</p>
     )
   }
 }
